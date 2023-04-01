b63d283887fc168fb6895949fc40dfc826fcd680
Bnyro <bnyro@tutanota.com>
2023-04-01 19:43:03 +0200

    Initial Commit

---
+++ main.c
@@ -0,0 +1,102 @@
+#include <gtk/gtk.h>
+#include <stdio.h>
+#include <string.h>
+
+GtkEntryBuffer *buffer;
+
+static void calculate (GtkWidget *widget, gpointer data)
+{
+  g_print ("Hello World\n");
+}
+
+static void update_text (GtkWidget *widget, gpointer data)
+{
+  char *label = gtk_button_get_label(GTK_BUTTON(widget));
+  char *current_text = gtk_entry_buffer_get_text(buffer);
+
+  if (strcmp(label, "C") == 0) {
+    gtk_entry_buffer_set_text(buffer, "", -1);
+  } else if (strcmp(label, "=") == 0) {
+    char* str;
+    int result = calculate_result(current_text);
+    asprintf(&str, "%i", result);
+    gtk_entry_buffer_set_text(buffer, str, -1);
+  } else {
+    char buf[256];
+    snprintf(buf, sizeof(buf), "%s%s", current_text, label);
+    gtk_entry_buffer_set_text(buffer, buf, -1);
+  }
+}
+
+static void activate (GtkApplication *app, gpointer user_data)
+{
+  GtkWidget *window = gtk_application_window_new (app);
+  gtk_window_set_title (GTK_WINDOW (window), "Window");
+  gtk_window_set_default_size (GTK_WINDOW (window), 200, 200);
+
+  GtkWidget *main_container = gtk_box_new(GTK_ORIENTATION_VERTICAL, 5);
+
+  buffer = gtk_entry_buffer_new ("", -1);
+  GtkWidget *entry = gtk_entry_new_with_buffer(buffer);
+  gtk_editable_set_editable(GTK_EDITABLE(entry), FALSE);
+
+  GtkWidget *button = gtk_button_new_with_label ("Calculate");
+  g_signal_connect (button, "clicked", G_CALLBACK (calculate), NULL);
+
+  GtkWidget *number_grid = gtk_grid_new();
+
+  for (int i = 1; i <= 9; i++)
+  {
+    pack_number_button(number_grid, i);
+  }
+
+  char *ops = "/x-+";
+
+  for (int i = 0; i < strlen(ops); i++) {
+    char *s = ops[i];
+    pack_button(number_grid, &s, 4, i, 1);
+  }
+
+  pack_button(number_grid, "C", 0, 0, 3);
+  pack_button(number_grid, "0", 0, 5, 2);
+  pack_button(number_grid, ".", 2, 5, 1);
+  pack_button(number_grid, "=", 4, 5, 1);
+
+  gtk_box_append(GTK_BOX(main_container), button);
+  gtk_box_append(GTK_BOX(main_container), entry);
+  gtk_box_append(GTK_BOX(main_container), number_grid);
+
+  gtk_window_set_child (GTK_WINDOW (window), main_container);
+  gtk_window_present (GTK_WINDOW (window));
+}
+
+void pack_number_button(GtkWidget *grid, int number)
+{
+  char* str;
+  asprintf(&str, "%i", number);
+  pack_button(grid, str, (number - 1) % 3, (9 - number) / 3 + 1, 1);
+}
+
+void pack_button(GtkWidget *grid, char *text, int column, int row, int weight)
+{
+  GtkWidget *button = gtk_button_new_with_label(text);
+  g_signal_connect (button, "clicked", G_CALLBACK (update_text), NULL);
+  gtk_grid_attach(GTK_GRID(grid), button, column, row, weight, 1);
+}
+
+int calculate_result(char *expr) {
+  return 0;
+}
+
+int main (int argc, char **argv)
+{
+  GtkApplication *app;
+  int status;
+
+  app = gtk_application_new ("com.bnyro.calculate", G_APPLICATION_DEFAULT_FLAGS);
+  g_signal_connect (app, "activate", G_CALLBACK (activate), NULL);
+  status = g_application_run (G_APPLICATION (app), argc, argv);
+  g_object_unref (app);
+
+  return status;
+}
